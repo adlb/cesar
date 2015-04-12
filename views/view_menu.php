8bf0fbf71bc46cb19f4b37e79222f7528a1efb87
Alban de La Bretèche <a.delabreteche@criteo.com>
2015-04-12 22:41:04 +0200

    Change brand name by Home Glyphicon

--- views/view_menu.php
+++ views/view_menu.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <a class="navbar-brand" href="?controller=site">Brand</a>
+      <a class="navbar-brand" href="?controller=site"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
